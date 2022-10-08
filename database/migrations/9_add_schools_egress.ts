95272eb12253ffbf346cf60b7f49d221d8b0c2fc
Guilherme Iuri <guiliuri13@gmail.com>
2022-10-08 11:55:48 -0300

    add school relation

---
+++ database/migrations/9_add_schools_egress.ts
@@ -0,0 +1,26 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema';
+
+export default class Egress extends BaseSchema
+{
+    protected tableName = 'egress';
+
+    public async up ()
+    {
+        this.schema.alterTable(this.tableName, (table) =>
+        {
+            table
+                .integer('school_id')
+                .unsigned()
+                .references('id')
+                .inTable('schools')
+                .onDelete('CASCADE');
+
+            table.dateTime('updated_at').nullable();
+        });
+    }
+
+    public async down ()
+    {
+        this.schema.dropTable(this.tableName);
+    }
+}
