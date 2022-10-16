699fc84bf8da5f289bc761d4943dc6ae8de4b09e
Guilherme Iuri <71740621+guiliuri13@users.noreply.github.com>
2022-10-16 02:46:44 -0300

    add first access

--- database/migrations/1_users.ts
+++ database/migrations/1_users.ts
@@ -12,6 +12,7 @@ export default class Users extends BaseSchema
             table.string('full_name', 255).notNullable();
             table.string('password', 180).notNullable();
             table.string('email', 255).notNullable().unique();
+            table.dateTime('first_access').nullable().defaultTo(null);
         });
     }
 
