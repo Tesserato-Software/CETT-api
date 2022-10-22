6d545c573a431daa568ce9657e60016a6daffb1d
Guilherme Iuri <71740621+guiliuri13@users.noreply.github.com>
2022-10-22 18:12:13 -0300

    fix ts errors to build

---
+++ app/Models/User.ts
@@ -0,0 +1,10 @@
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm';
+
+export default class User extends BaseModel
+{
+    @column({ isPrimary: true })
+    public id?: number;
+
+    @column()
+    public email?: string;
+}
