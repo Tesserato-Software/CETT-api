3b38a98cd48606230baa9797850c63cb999be05d
Guilherme Iuri <guiliuri13@gmail.com>
2022-11-07 19:11:42 -0300

    fix attach and dettach

--- start/routes/Egress.ts
+++ start/routes/Egress.ts
@@ -3,7 +3,7 @@ import Route from '@ioc:Adonis/Core/Route';
 /* --------------------------------- Egress ----------------------------------- */
 Route.group(() =>
 {
-    Route.patch('dettach_archives', 'EgressController.DettachArchives');
+    Route.patch('dettach-archives', 'EgressController.DettachArchives');
     Route.patch('attach-archives/:id', 'EgressController.AttachArchives');
     Route.get('dashboard-attach-archive', 'EgressController.DashboardAttachArchive');
 }).prefix('egress').middleware(['auth']);
