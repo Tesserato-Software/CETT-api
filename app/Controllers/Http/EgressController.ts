c2bbc956327398bc31b64769865daf8bc78b92cf
Guilherme Iuri <71740621+guiliuri13@users.noreply.github.com>
2022-11-09 21:04:44 -0300

    done egress CRUD ✅

--- app/Controllers/Http/EgressController.ts
+++ app/Controllers/Http/EgressController.ts
@@ -1,9 +1,272 @@
 /* eslint-disable one-var */
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import Database from '@ioc:Adonis/Lucid/Database';
+import ListValidator from 'App/Validators/ListValidator';
+import { DateTime } from 'luxon';
 
 export default class EgressController
 {
+    public async index ({ response, auth, request }: HttpContextContract)
+    {
+        let { user } = auth;
+
+        if (!user)
+        {
+            return response.unauthorized({ message: 'Unauthorized' });
+        }
+
+        try
+        {
+            let pagination;
+
+            if(request.method() === 'POST')
+            {
+                pagination = await (await request.validate(ListValidator)).pagination;
+            }
+
+            let query = await Database
+                .from('egresses')
+                .select('egresses.*')
+                .where('egresses.school_id', user.school_id)
+                .groupBy('egresses.id')
+                .if(pagination, query =>
+                {
+                    query.paginate(pagination?.page, pagination?.per_page_limit);
+                });
+
+            return response.ok(query);
+        }
+        catch (error)
+        {
+            console.error(error);
+            return response.internalServerError({ error });
+        }
+    }
+
+    public async store ({ response, auth, request }: HttpContextContract)
+    {
+        let { user } = auth;
+
+        if (!user)
+        {
+            return response.unauthorized({ message: 'Unauthorized' });
+        }
+
+        let {
+            name,
+            CGM_id,
+            arq_id,
+            birth_date,
+            responsible_name,
+            archive_id,
+        } = request.all();
+
+        if (!name || typeof name !== 'string')
+        {
+            return response.badRequest('name must be a string');
+        }
+
+        if (CGM_id && typeof CGM_id !== 'number')
+        {
+            return response.badRequest('CGM_id must be a number');
+        }
+
+        if (birth_date && DateTime.fromISO(birth_date).isValid === false)
+        {
+            return response.badRequest('birth_date must be a valid date');
+        }
+
+        if (archive_id)
+        {
+            try
+            {
+                await Database
+                    .from('archives')
+                    .where('id', archive_id)
+                    .firstOrFail();
+            }
+            catch (error)
+            {
+                return response.badRequest('archive_id not found');
+            }
+        }
+
+        try
+        {
+            let egress = await Database
+                .insertQuery()
+                .table('egresses')
+                .returning('id')
+                .insert({
+                    name,
+                    CGM_id,
+                    arq_id,
+                    birth_date,
+                    responsible_name,
+                    archive_id,
+                    school_id: user.school_id,
+                });
+
+            return response.ok({ egress });
+        }
+        catch (error)
+        {
+            console.error(error);
+            return response.internalServerError({ error });
+        }
+    }
+
+    public async update ({ response, auth, request, params }: HttpContextContract)
+    {
+        let { user } = auth,
+            egress_id = params.id;
+
+        if (!egress_id)
+        {
+            return response.badRequest('egress id must be a number');
+        }
+
+        if (!user || !user.hierarchy_id)
+        {
+            return response.unauthorized({ message: 'Unauthorized' });
+        }
+        else if (user && user.hierarchy_id)
+        {
+            try
+            {
+                let hierarchy = await Database
+                    .from('hierarchies')
+                    .where('id', user.hierarchy_id)
+                    .firstOrFail();
+
+                if (!hierarchy?.can_update)
+                {
+                    return response.unauthorized({ message: 'Unauthorized' });
+                }
+            }
+            catch (error)
+            {
+                console.error(error);
+                return response.internalServerError({ error });
+            }
+        }
+
+        let {
+            name,
+            CGM_id,
+            arq_id,
+            birth_date,
+            responsible_name,
+            archive_id,
+        } = request.all();
+
+        if (!name || typeof name !== 'string')
+        {
+            return response.badRequest('name must be a string');
+        }
+
+        if (CGM_id && typeof CGM_id !== 'number')
+        {
+            return response.badRequest('CGM_id must be a number');
+        }
+
+        if (birth_date && DateTime.fromISO(birth_date).isValid === false)
+        {
+            return response.badRequest('birth_date must be a valid date');
+        }
+
+        if (archive_id)
+        {
+            try
+            {
+                await Database
+                    .from('archives')
+                    .where('id', archive_id)
+                    .firstOrFail();
+            }
+            catch (error)
+            {
+                return response.badRequest('archive_id not found');
+            }
+        }
+
+        try
+        {
+            let egress = await Database
+                .query()
+                .from('egresses')
+                .where('id', egress_id)
+                .update({
+                    name,
+                    CGM_id,
+                    arq_id,
+                    birth_date,
+                    responsible_name,
+                    archive_id,
+                    last_edit_by: user.id,
+                    updated_at: DateTime.now(),
+                });
+
+            return response.ok({ egress });
+        }
+        catch (error)
+        {
+            console.error(error);
+            return response.internalServerError({ error });
+        }
+    }
+
+    public async destroy ({ response, auth, params }: HttpContextContract)
+    {
+        let { user } = auth,
+            egress_id = params.id;
+
+        if (!egress_id)
+        {
+            return response.badRequest('egress id must be a number');
+        }
+
+        if (!user || !user.hierarchy_id)
+        {
+            return response.unauthorized({ message: 'Unauthorized' });
+        }
+        else if (user && user.hierarchy_id)
+        {
+            try
+            {
+                let hierarchy = await Database
+                    .from('hierarchies')
+                    .where('id', user.hierarchy_id)
+                    .firstOrFail();
+
+                if (!hierarchy?.can_delete)
+                {
+                    return response.unauthorized({ message: 'Unauthorized' });
+                }
+            }
+            catch (error)
+            {
+                console.error(error);
+                return response.internalServerError({ error });
+            }
+        }
+
+        try
+        {
+            let egress = await Database
+                .from('egresses')
+                .where('id', egress_id)
+                .delete();
+
+            return response.ok({ egress });
+        }
+        catch (error)
+        {
+            console.error(error);
+            return response.internalServerError({ error });
+        }
+    }
+
     public async DashboardAttachArchive ({ response, auth }: HttpContextContract)
     {
         let { user } = auth;
@@ -101,4 +364,3 @@ export default class EgressController
         }
     }
 }
-
--- start/routes/Egress.ts
+++ start/routes/Egress.ts
@@ -3,8 +3,19 @@ import Route from '@ioc:Adonis/Core/Route';
 /* --------------------------------- Egress ----------------------------------- */
 Route.group(() =>
 {
+    // POST
+    Route.post('create', 'EgressController.store');
+    Route.post('list', 'EgressController.index');
+    Route.post('update/:id', 'EgressController.update');
+
+    // GET
+    Route.get('dashboard-attach-archive', 'EgressController.DashboardAttachArchive');
+    Route.get('list', 'EgressController.index');
+
+    // DELETE
+    Route.delete('delete/:id', 'EgressController.destroy');
+
+    // PATCH
     Route.patch('dettach-archives', 'EgressController.DettachArchives');
     Route.patch('attach-archives/:id', 'EgressController.AttachArchives');
-    Route.get('dashboard-attach-archive', 'EgressController.DashboardAttachArchive');
 }).prefix('egress').middleware(['auth']);
-
