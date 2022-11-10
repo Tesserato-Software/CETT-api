ae9d901ed8fbd875744c5eb9bfd3e9407731e89c
zesanches <josemanoelmalassise@gmail.com>
2022-11-09 21:16:42 -0300

    add create, list and update user

--- app/Controllers/Http/UserController.ts
+++ app/Controllers/Http/UserController.ts
@@ -6,16 +6,16 @@ export default class UserController
 {
     public async AttachHierarchy ({ response, auth, request, params }: HttpContextContract)
     {
-        let { user:currentuser } = auth,
+        let { user: currentuser } = auth,
             hierarchy_id = params.id,
             { users } = request.all();
 
-        if(!currentuser)
+        if (!currentuser)
         {
             return response.unauthorized({ message: 'Unauthorized' });
         }
 
-        if (!users.length || users.find(u => typeof u !== 'number'))
+        if (!users.length || users.find((u) => typeof u !== 'number'))
         {
             return response.badRequest('user must be a number array');
         }
@@ -31,10 +31,7 @@ export default class UserController
             {
                 try
                 {
-                    await Database
-                        .from('users')
-                        .where('id', user)
-                        .firstOrFail();
+                    await Database.from('users').where('id', user).firstOrFail();
                 }
                 catch (error)
                 {
@@ -42,10 +39,7 @@ export default class UserController
                 }
             }
 
-            await Database
-                .from('users')
-                .whereIn('id', users)
-                .update({ hierarchy_id });
+            await Database.from('users').whereIn('id', users).update({ hierarchy_id });
 
             return response.ok('updated');
         }
@@ -55,5 +49,120 @@ export default class UserController
             return response.internalServerError({ error });
         }
     }
-}
 
+    public async ListUsers ({ response, auth }: HttpContextContract)
+    {
+        const { user } = auth;
+
+        if (!user)
+        {
+            return response.unauthorized({ message: 'Unauthorized' });
+        }
+
+        try
+        {
+            let users = await Database.from('users')
+                .select(
+                    'users.id',
+                    'users.full_name',
+                    'users.email',
+                    Database.raw(`
+                            json_agg(
+                                hierarchies.*
+                            ) AS hierarchy
+                    `)
+                )
+                .where('users.school_id', user.school_id)
+                .innerJoin('hierarchies', 'hierarchies.id', 'users.hierarchy_id')
+                .groupBy('users.id');
+
+            return response.ok(users);
+        }
+        catch (error)
+        {
+            return response.internalServerError({ message: 'Internal Server Error' });
+        }
+    }
+
+    public async CreateUsers ({ response, auth, request }: HttpContextContract)
+    {
+        const { user } = auth;
+        const { email, full_name, password, role } = request.all();
+        const isInvalidRegister = !(
+            email?.length ||
+            full_name?.length ||
+            password?.length ||
+            role
+        );
+
+        if (!user)
+        {
+            return response.unauthorized({ message: 'Unauthorized' });
+        }
+
+        if (isInvalidRegister)
+        {
+            return response.badRequest({ message: 'Bad Request' });
+        }
+
+        try
+        {
+            await Database.table('users')
+                .returning('id')
+                .insert({
+                    full_name,
+                    email,
+                    password,
+                    hierarchy_id: role,
+                });
+
+            return response.ok('updated');
+        }
+        catch (error)
+        {
+            return response.internalServerError({ message: 'Internal Server Error' });
+        }
+    }
+
+    public async UpdateUsers ({ response, auth, request, params }: HttpContextContract)
+    {
+        const { user } = auth;
+        const user_id = params.id;
+        const { email, full_name, password, role, school_id } = request.all();
+        const isInvalidRegister = !(
+            school_id ||
+            email?.length ||
+            full_name?.length ||
+            password?.length ||
+            role
+        );
+
+        if (!user)
+        {
+            return response.unauthorized({ message: 'Unauthorized' });
+        }
+
+        if (isInvalidRegister)
+        {
+            return response.badRequest({ message: 'Bad Request' });
+        }
+
+        try
+        {
+            await Database.from('users')
+                .where('id', user_id)
+                .update({
+                    full_name,
+                    password,
+                    email,
+                    hierarchy_id: role,
+                });
+
+            return response.ok('updated');
+        }
+        catch (error)
+        {
+            return response.internalServerError({ message: 'Internal Server Error' });
+        }
+    }
+}
--- start/routes/User.ts
+++ start/routes/User.ts
@@ -4,6 +4,12 @@ import Route from '@ioc:Adonis/Core/Route';
 
 Route.group(() =>
 {
+    // GET / list users
+    Route.get('list', 'UserController.ListUsers');
+    // POST / create users
+    Route.post('create', 'UserController.CreateUsers');
+    // PUT / update users
+    Route.put('update/:id', 'UserController.UpdateUsers');
     // GET /user
     Route.post('user-hierarchy/:id', 'UserController.AttachHierarchy');
 }).prefix('user').middleware(['auth']);
