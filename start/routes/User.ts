b7ef917756567776d67b9511f70e2dabc49c0ee9
zesanches <josemanoelmalassise@gmail.com>
2022-11-11 21:09:55 -0300

    add route to user delete

--- start/routes/User.ts
+++ start/routes/User.ts
@@ -10,6 +10,8 @@ Route.group(() =>
     Route.post('create', 'UserController.CreateUsers');
     // PUT / update users
     Route.put('update/:id', 'UserController.UpdateUsers');
+    // DELETE / delete users
+    Route.delete('delete/:id', 'UserController.DeleteUsers');
     // GET /user
     Route.post('user-hierarchy/:id', 'UserController.AttachHierarchy');
     Route.post('psw-fa', 'UserController.PswFirstAccess');
