417f0dbf89cfc90e9df15cb51e92b29f83bbbf9c
Guilherme Iuri <71740621+guiliuri13@users.noreply.github.com>
2022-11-11 21:03:51 -0300

    done bigode archives

--- app/Controllers/Http/ArchiveController.ts
+++ app/Controllers/Http/ArchiveController.ts
@@ -3,6 +3,111 @@ import Database from '@ioc:Adonis/Lucid/Database';
 
 export default class ArchiveController
 {
+    public async index ({ response, auth }: HttpContextContract)
+    {
+        let { user } = auth;
+
+        if (!user)
+        {
+            return response.unauthorized({ message: 'Unauthorized' });
+        }
+
+        try
+        {
+            let archives = await Database
+                .from('archives')
+                .select(
+                    'archives.school_id',
+                    'archives.id',
+                )
+                .where('archives.school_id', user.school_id)
+                .groupBy('archives.id');
+
+            return response.ok(archives);
+        }
+        catch (error)
+        {
+            console.error(error);
+            return response.internalServerError({ message: 'Internal Server Error' });
+        }
+    }
+
+    public async create ({ response, auth }: HttpContextContract)
+    {
+        let { user } = auth;
+
+        if(!user) //if u ser is not authenticated
+        {
+            return response.unauthorized({ message: 'Unauthorized' }); //retorna não autorizado
+        }
+        if(!user.school_id)
+        {
+            return response.unauthorized({ message: 'Unauthorized' }); //retorna não autorizado 
+        }
+        try
+        {
+            let archives = await Database //tenta inserir dados na tabela archives 
+                .insertQuery()
+                .table('archives')
+                .insert({school_id: user.school.id});
+
+            return response.ok({archives}); //retorna os dados criados
+        }
+        catch (error) // caso de algum erro 
+        {
+            console.error(error);
+            return response.internalServerError({ message: 'Internal Server Error' }); //retorna erro de sevidor interno
+        }
+    }
+
+    public async delete ({ response, auth, params }: HttpContextContract)
+    {
+        let { user } = auth,
+            archive_id = params.id;
+
+        if (!user || !user.hierarchy_id)
+        {
+            return response.unauthorized({ message: 'Unauthorized' });
+        }
+        else if (user && user.hierarchy_id)
+        {
+            try
+            {
+                let hierarchy = await Database.from('users')
+                    .where('id', user.hierarchy_id)
+                    .firstOrFail();
+
+                if (!hierarchy?.can_delete)
+                {
+                    return response.unauthorized({ message: 'Unauthorized'});
+                }
+            }
+            catch (error)
+            {
+                console.error(error);
+                return response.internalServerError({ error });
+            }
+        }
+
+        try
+        {
+            await Database.from('egress')
+                .where('archive_id', archive_id)
+                .update({ archive_id: null });
+
+            await Database.from('archives')
+                .where('id',archive_id)
+                .delete();
+
+            return response.ok({});
+        }
+        catch (error)
+        {
+            console.error(error);
+            return response.internalServerError({ error });
+        }
+    }
+
     public async DashBoardAttachEgress ({ response, auth, params}: HttpContextContract)
     {
         let { user } = auth,
--- start/routes/Archive.ts
+++ start/routes/Archive.ts
@@ -6,6 +6,13 @@ Route.group(() =>
     // GET /archive
     Route.get('list', 'ArchiveController.index');
     Route.get('dashboard-attach-egress/:id','ArchiveController.DashBoardAttachEgress');
+
+    // POST /archive
+    Route.post('create','ArchiveController.create');
+
+    // DELETE /archive
+    Route.delete('delete','ArchiveController.delete');
+
     // Patch /archive
     Route.patch('dettach-egress','ArchiveController.DettachEgress');
     Route.patch('attach-egress/:id','ArchiveController.AttachEgress');
