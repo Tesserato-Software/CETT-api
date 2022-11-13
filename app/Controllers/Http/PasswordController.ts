6c8de2c2516f37c236b177593c8a11c65b63c5de
Guilherme Iuri <71740621+guiliuri13@users.noreply.github.com>
2022-11-13 17:57:44 -0300

    DONE HASH PASS

--- app/Controllers/Http/PasswordController.ts
+++ app/Controllers/Http/PasswordController.ts
@@ -1,5 +1,4 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import Database from '@ioc:Adonis/Lucid/Database';
 import User from 'App/Models/User';
 
 export default class PasswordController
