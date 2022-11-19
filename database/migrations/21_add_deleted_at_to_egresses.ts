a0d6550502e6db25c8165f29782759b3080f9891
Guilherme Iuri <71740621+guiliuri13@users.noreply.github.com>
2022-11-19 16:20:54 -0300

    soft delete

---
+++ database/migrations/21_add_deleted_at_to_egresses.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema';
+
+export default class Egresses extends BaseSchema
+{
+    protected tableName = 'egresses';
+
+    public async up ()
+    {
+        this.schema.alterTable(this.tableName, (table) =>
+        {
+            table
+                .dateTime('deleted_at')
+                .nullable()
+                .defaultTo(null);
+        });
+    }
+
+    public async down ()
+    {
+        this.schema.dropTable(this.tableName);
+    }
+}
